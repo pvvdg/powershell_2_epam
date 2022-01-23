92127a9e78a31c6bee62115f85cd4e192049ff4f
pvvdg <vladpetrachuk@gmail.com>
2022-01-23 12:37:41 +0300

    feat: add solution 1.1

---
+++ 1_1.ps1
@@ -0,0 +1,26 @@
+# 1.	Даны два целых числа, каждое число записано в отдельной строке (string). Выведите наибольшее из данных чисел.
+
+$number1 = "5"
+$number2 = "4"
+
+# switch ($true) {
+#     ($number1 -gt $number2) { 
+#         Write-Host "$number1 greater than $number2"
+#     }
+#      ($number1 -lt $number2) { 
+#         Write-Host "$number1 less than $number2"
+#     }
+#     Default {
+#         Write-Host "Numbers equal"
+#     }
+# }
+
+if ($number1 -gt $number2) {
+    Write-Host "$number1 greater than $number2"
+}
+elseif ($number1 -lt $number2) {
+    Write-Host "$number1 less than $number2"
+}
+else {
+    Write-Host "Numbers equal"
+}
