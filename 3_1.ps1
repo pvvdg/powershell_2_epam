8532267fd677b93b981cc1725f3623fc853cb92c
pvvdg <vladpetrachuk@gmail.com>
2022-01-24 12:55:26 +0300

    feat: add one more solution

--- 3_1.ps1
+++ 3_1.ps1
@@ -1,11 +1,25 @@
 
 # 1.	Посчитать сумму длин файлов log и txt на диске С: (обязательно циклом foreach, в gci не фильтровать)
-$FilesLength = Get-ChildItem -Force -Path C:\Task\* -Include "*.txt", "*.log" | Select-Object -Expand Length
+# SIMPLE SOLUTION
+
+# $FilesLength = Get-ChildItem -Force -Path C:\Task\* -Include "*.txt", "*.log" | Select-Object -Expand Length
+
+# $Sum = 0
+
+# foreach ($FileLength in $FilesLength) {
+#     $Sum += $FileLength
+# }
+
+# $Sum
+
+$Files = Get-ChildItem -Force -Path C:\Task\
 
 $Sum = 0
 
-foreach ($FileLength in $FilesLength) {
-    $Sum += $FileLength
+foreach ($File in $Files) {
+    if ($File.Name -Match ".txt" -or $File.Name -Match ".log") {
+        $Sum += $File.Length
+    }
 }
 
-Write-Host $Sum
+$Sum
