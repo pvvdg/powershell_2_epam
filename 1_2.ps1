091f974e812d0612dc82c184228bdf8b3cdaddad
pvvdg <vladpetrachuk@gmail.com>
2022-01-25 13:47:08 +0300

    fix: change solution for ps 5

--- 1_2.ps1
+++ 1_2.ps1
@@ -1,6 +1,14 @@
 # 2.	Требуется определить, является ли год високосным. Номер года указывается в отдельной переменной.
 #  (Напомним, что год является високосным, если его номер кратен 4, но не кратен 100, а также если он кратен 400.)
 
-$year = 1700
+$year = 2019
 
-(($year % 4 -eq 0) -and ($year % 100 -ne 0)) -or ($year % 400 -eq 0) ? "$year leap year" : "$year not a leap year"
+#For PS 7 and over
+#(($year % 4 -eq 0) -and ($year % 100 -ne 0)) -or ($year % 400 -eq 0) ? "$year leap year" : "$year not a leap year"
+
+if ((($year % 4 -eq 0) -and ($year % 100 -ne 0)) -or ($year % 400 -eq 0)) {
+    Write-Host "$year leap year"
+}
+else {
+    Write-Host "$year not a leap year"
+}
